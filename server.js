cd54aa5b8027541f48121f9963cfffcb4265e0f0
Atkenny <tellezkenny08@gmail.com>
2024-06-29 17:32:49 -0600

    Modificacion a server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mysql = require('mysql');
+// const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000; // Usamos process.env.PORT para el puerto si está configurado, de lo contrario, el puerto 5000
+const port = 5000;
 
 // Middleware para analizar el cuerpo de la solicitud en formato JSON
 app.use(express.json({ limit: '50mb' }));
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: 'bmo0fw8udjc5rzwzhgsv-mysql.services.clever-cloud.com',
+  user: 'ujpsbjf47zw4rncw',
+  password: 'ZzAxCYcaomg2b6pkf7QQ',
+  // password: '2105Oney',
+  database: 'bmo0fw8udjc5rzwzhgsv'
 });
 
 db.connect((err) => {
@@ -20,6 +22,23 @@ db.connect((err) => {
     console.error('Error de conexión a la base de datos:', err);
   } else {
     console.log('Conexión exitosa a la base de datos');
+  }
+});
+
+// Configuración de la conexión a la segunda base de datos
+const db2 = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '@kekodroid',
+  // password: '2105Oney',
+  database: 'datamart'
+});
+
+db2.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la segunda base de datos:', err);
+  } else {
+    console.log('Conexión exitosa a la segunda base de datos');
   }
 });
 
@@ -32,6 +51,11 @@ app.listen(port, () => {
 // Importar rutas CRUD y pasarlas a la aplicación
 const crudRoutes = require('./routes/crudRoutes.js')(db);
 app.use('/crud', crudRoutes);
+
+// Importar y usar rutas para la segunda base de datos
+const crudRoutes2 = require('./routes/crudRoutes2')(db2); // Pasa la instancia de la segunda base de datos a crudRoutesDb2
+app.use('/crud2', crudRoutes2);
+
 
 // Middleware para manejar errores de análisis JSON
 app.use((err, req, res, next) => {
